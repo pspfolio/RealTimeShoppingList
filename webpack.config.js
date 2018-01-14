91481aed97433faf8b74b668f3eb9b0339eda70b
pspfolio <pspfolio@gmail.com>
2018-01-14 15:14:13 +0200

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  // polku josta React aloittaa suorituksen
+  entry: './app/index.jsx',
+  output: {
+    // polku johon bundlattu tiedosto generoidaan ja bundlen nimi
+    path: path.resolve('public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    publicPath: '/public/'
+  },
+  // Kerrotaan webpackille, että selvittää automaattisesti nämä päätteet
+  // Ei tarvitse kirjoittaa esim importissa tiedoston päätettä
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  stats: {
+    colors: true,
+    reasons: true
+  },
+  module: {
+    // loadereita tarvitaan jotta voidaan käyttää ES6 ja JSX koodia
+    // babel-loaderia käytetään jokaisessa tiedostossa jotka päättyvät .js tai .jsx
+    loaders: [
+      { test: /\.js$/, loader: ['babel-loader', 'eslint-loader'], exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: ['babel-loader', 'eslint-loader'], exclude: /node_modules/ }
+    ]
+  }
+};
