4abc6d89a9a455cfa74aaee1bf4b460ffef20d8f
pspfolio <pspfolio@gmail.com>
2018-01-17 22:06:01 +0200

    setting up basic firebase email and password authentication

---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyDd88kI7ssd8pE6GL1GO7vfPZo7xplQ17g',
+  authDomain: 'shoplist-adc45.firebaseapp.com',
+  databaseURL: 'https://shoplist-adc45.firebaseio.com',
+  projectId: 'shoplist-adc45',
+  storageBucket: 'shoplist-adc45.appspot.com',
+  messagingSenderId: '129365943037'
+};
+
+if (!firebase.apps.length) firebase.initializeApp(config);
+
+const auth = firebase.auth();
+
+export default auth;
---
+++ src/firebase/index.js
@@ -0,0 +1,4 @@
+import * as auth from './auth';
+import firebase from './firebase';
+
+export { auth, firebase };
