cf4461ea4e68d9ce2c2a9e2cca643f31ebc6f9ab
pspfolio <pspfolio@gmail.com>
2018-01-19 18:14:12 +0200

    Signup form to work

--- src/features/Authentication/SignUp.jsx
+++ src/features/Authentication/SignUp.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import SignUpForm from './SignUpForm';
 
 const SignUp = () => (
   <div>
     <h1>SignUp</h1>
+    <SignUpForm />
   </div>
 );
 
--- src/features/Authentication/SignUpForm.jsx
+++ src/features/Authentication/SignUpForm.jsx
@@ -1,18 +1,88 @@
 import React, { Component } from 'react';
+import { auth } from '../../firebase';
 
 class SignUpForm extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      username: '',
+      password: '',
+      email: '',
+      confirmPassword: '',
+      error: null
+    };
+
     this.onSubmit = this.onSubmit.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
   }
 
-  onSubmit() {
-    console.log('loog');
+  onSubmit(event) {
+    const { email, password } = this.state;
+    auth
+      .doCreateUserWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({
+          username: '',
+          password: '',
+          email: '',
+          confirmPassword: '',
+          error: null
+        });
+      })
+      .catch(error =>
+        this.setState({
+          error
+        })
+      );
+
+    event.preventDefault();
+  }
+
+  onInputChange(key, value) {
+    this.setState({
+      [key]: value
+    });
   }
 
   render() {
-    return <form onSubmit={this.onSubmit} />;
+    const { username, email, password, confirmPassword, error } = this.state;
+
+    const isInvalid = password !== confirmPassword || password === '' || email === '' || username === '';
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          value={username}
+          onChange={event => this.onInputChange('username', event.target.value)}
+          type="text"
+          placeholder="username"
+        />
+        <input
+          value={email}
+          onChange={event => this.onInputChange('email', event.target.value)}
+          type="text"
+          placeholder="email"
+        />
+        <input
+          value={password}
+          onChange={event => this.onInputChange('password', event.target.value)}
+          type="password"
+          placeholder="password"
+        />
+        <input
+          value={confirmPassword}
+          onChange={event => this.onInputChange('confirmPassword', event.target.value)}
+          type="password"
+          placeholder="confirm password"
+        />
+        <button disable={isInvalid} type="submit">
+          Sign up
+        </button>
+
+        {error && <p>{error.message}</p>}
+      </form>
+    );
   }
 }
 
