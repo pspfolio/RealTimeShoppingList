945a3182b2e4579457df15427ae36ee076638ce0
pspfolio <pspfolio@gmail.com>
2018-01-21 19:16:18 +0200

    Sign-in form component and implementation

---
+++ src/features/Authentication/SignInForm.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { auth } from '../../firebase';
+
+const InitialState = {
+  email: '',
+  password: '',
+  error: null
+};
+
+class SignInForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...InitialState };
+  }
+
+  onSubmit(event) {
+    const { email, password } = this.state;
+    const { history } = this.props;
+
+    auth
+      .doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({ ...InitialState });
+        history.push('/');
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+
+    event.preventDefault();
+  }
+
+  render() {
+    const { email, password, error } = this.state;
+    const isInvalid = password === '' || email === '';
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          value={email}
+          onChange={event => this.setState({ email: event.target.value })}
+          type="text"
+          placeholder="Email address"
+        />
+        <input
+          value={password}
+          onChange={event => this.setState({ password: event.target.value })}
+          type="password"
+          placeholder="Password"
+        />
+        <button type="submit" disabled={isInvalid}>
+          Sign in
+        </button>
+
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
+}
+
+export default SignInForm;
--- src/features/Authentication/SignUpLink.jsx
+++ src/features/Authentication/SignUpLink.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const SignUpLink = () => (
   <p>
-    Dont have an account? <Link to={'/register'}>Sign up</Link>
+    Dont have an account? <Link to="/register">Sign up</Link>
   </p>
 );
 
