177b1ad8b84b1e68f358abfee2f925f1de01ecbb
pspfolio <pspfolio@gmail.com>
2018-01-21 21:44:55 +0200

    app component to handle authUser object and initializing authChanged listener

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import styled from 'styled-components';
+import { firebase } from './firebase';
 import Shoplist from './features/Shoplists/Shoplist';
 import Shoplists from './features/Shoplists/Shoplists';
 import SignIn from './features/Authentication/SignIn';
@@ -13,17 +14,39 @@ const FlexWrapper = styled.div`
   justify-content: center;
 `;
 
-const App = () => (
-  <Router>
-    <FlexWrapper>
-      <SignOut />
-      <Route exact path="/" component={Shoplist} />
-      <Route exact path="/list" component={Shoplists} />
-      <Route exact path="/login" component={SignIn} />
-      <Route exact path="/register" component={SignUp} />
-      <Route exact path="/pw-forget" component={PasswordForget} />
-    </FlexWrapper>
-  </Router>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authUser: null
+    };
+  }
+
+  componentDidMount() {
+    firebase.auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        this.setState(() => ({ authUser }));
+      } else {
+        this.setState(() => ({ authUser: null }));
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Router>
+        <FlexWrapper>
+          <SignOut />
+          <Route exact path="/" component={Shoplist} />
+          <Route exact path="/list" component={Shoplists} />
+          <Route exact path="/login" component={SignIn} />
+          <Route exact path="/register" component={SignUp} />
+          <Route exact path="/pw-forget" component={PasswordForget} />
+        </FlexWrapper>
+      </Router>
+    );
+  }
+}
 
 export default App;
