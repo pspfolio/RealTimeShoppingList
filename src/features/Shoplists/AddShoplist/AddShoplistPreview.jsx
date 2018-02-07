e2fc4d95827196b167de3afc8980fdab29d161ee
pspfolio <pspfolio@gmail.com>
2018-02-07 18:41:48 +0200

    writing shoplistpreview tests

--- src/features/Shoplists/AddShoplist/AddShoplistPreview.jsx
+++ src/features/Shoplists/AddShoplist/AddShoplistPreview.jsx
@@ -5,10 +5,14 @@ const ShoplistItem = styled.p`
   color: #5e7785;
 `;
 
-const AddShoplistPreview = ({ items, categoryName }) => (
+const AddShoplistPreview = ({ items = [], categoryName }) => (
   <div>
-    <h3>{`${categoryName} `}</h3>
-    {items.map(item => <ShoplistItem key={item}>{item}</ShoplistItem>)}
+    <h3>{`${categoryName}`}</h3>
+    {items.map(item => (
+      <ShoplistItem className="preview-shoplist-item" key={item}>
+        {item}
+      </ShoplistItem>
+    ))}
   </div>
 );
 
--- src/features/Shoplists/AddShoplist/AddShoplistPreview.test.js
+++ src/features/Shoplists/AddShoplist/AddShoplistPreview.test.js
@@ -1,1 +1,20 @@
-test('it works', () => {});
+import React from 'react';
+import { shallow } from 'enzyme';
+import AddShoplistPreview from './AddShoplistPreview';
+
+test('renders component', () => {
+  const wrapper = shallow(<AddShoplistPreview />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('renders the category name', () => {
+  const categoryName = 'Test category';
+  const wrapper = shallow(<AddShoplistPreview categoryName={categoryName} />);
+  expect(wrapper.find('h3').text()).toEqual(categoryName);
+});
+
+test('Preview should render correct amount of Shoplist items', () => {
+  const items = ['test1', 'test2'];
+  const wrapper = shallow(<AddShoplistPreview items={items} />);
+  expect(wrapper.find('.preview-shoplist-item').length).toEqual(items.length);
+});
