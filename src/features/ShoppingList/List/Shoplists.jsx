ac991f0ff5ba7de126d0021058538b653c2d2e36
pspfolio <pspfolio@gmail.com>
2018-02-20 22:40:37 +0200

    lodash map and basic setup for shoppinglist list

--- src/features/ShoppingList/List/Shoplists.jsx
+++ src/features/ShoppingList/List/Shoplists.jsx
@@ -1,4 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import map from 'lodash/map';
+import { database } from '../../../firebase/index';
 
-const Shoplists = () => <h1>SHOPLISTS</h1>;
+class Shoplists extends Component {
+  constructor() {
+    super();
+    this.state = {
+      shoplists: null
+    };
+    this.shoppinglistRef = database.ref('shoppinglist');
+  }
+
+  componentDidMount() {
+    this.shoppinglistRef.on('value', snapshot => {
+      this.setState({
+        shoplists: snapshot.val().shoplist
+      });
+    });
+  }
+
+  render() {
+    const { shoplists } = this.state;
+    return <div>{map(shoplists, (shoplist, key) => <p key={key}>{shoplist}</p>)}</div>;
+  }
+}
 export default Shoplists;
